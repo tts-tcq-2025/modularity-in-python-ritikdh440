6981ac3e945faaca0dc70ebde4fe6d9ab04f3c2a
Ritik dhiman <57491718+ritikdh440@users.noreply.github.com>
2025-08-12 09:12:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,40 @@
+from color_coder import get_color_from_pair_number, get_pair_number_from_color
 
-MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
-MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
-
-
-def color_pair_to_string(major_color, minor_color):
-  return f'{major_color} {minor_color}'
-
-
-def get_color_from_pair_number(pair_number):
-  zero_based_pair_number = pair_number - 1
-  major_index = zero_based_pair_number // len(MINOR_COLORS)
-  if major_index >= len(MAJOR_COLORS):
-    raise Exception('Major index out of range')
-  minor_index = zero_based_pair_number % len(MINOR_COLORS)
-  if minor_index >= len(MINOR_COLORS):
-    raise Exception('Minor index out of range')
-  return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
+def print_reference_manual():
+    """
+    Formats and prints a reference manual of the 25-pair color code.
+    The output is a table mapping pair numbers to their color combinations.
+    """
+    print("--- 25-Pair Color Code Reference Manual ---")
+    print("{:<12} | {:<12} | {:<12}".format("Pair Number", "Major Color", "Minor Color"))
+    print("-" * 40)
+    
+    for i in range(1, 26):
+        major, minor = get_color_from_pair_number(i)
+        print("{:<12} | {:<12} | {:<12}".format(i, major, minor))
+    print("-" * 40)
 
 
-def get_pair_number_from_color(major_color, minor_color):
-  try:
-    major_index = MAJOR_COLORS.index(major_color)
-  except ValueError:
-    raise Exception('Major index out of range')
-  try:
-    minor_index = MINOR_COLORS.index(minor_color)
-  except ValueError:
-    raise Exception('Minor index out of range')
-  return major_index * len(MINOR_COLORS) + minor_index + 1
+def test_number_to_pair(pair_number, expected_major, expected_minor):
+    """Tests the conversion from a pair number to colors."""
+    major, minor = get_color_from_pair_number(pair_number)
+    assert major == expected_major
+    assert minor == expected_minor
 
 
-def test_number_to_pair(pair_number,
-                        expected_major_color, expected_minor_color):
-  major_color, minor_color = get_color_from_pair_number(pair_number)
-  assert(major_color == expected_major_color)
-  assert(minor_color == expected_minor_color)
-
-
-def test_pair_to_number(major_color, minor_color, expected_pair_number):
-  pair_number = get_pair_number_from_color(major_color, minor_color)
-  assert(pair_number == expected_pair_number)
+def test_pair_to_number(major, minor, expected_pair_number):
+    """Tests the conversion from a color pair to its number."""
+    pair_number = get_pair_number_from_color(major, minor)
+    assert pair_number == expected_pair_number
 
 
 if __name__ == '__main__':
-  test_number_to_pair(4, 'White', 'Brown')
-  test_number_to_pair(5, 'White', 'Slate')
-  test_pair_to_number('Black', 'Orange', 12)
-  test_pair_to_number('Violet', 'Slate', 25)
-  test_pair_to_number('Red', 'Orange', 7)
-  print('Done :)')
+    # --- Run Tests ---
+    test_number_to_pair(4, 'White', 'Brown')
+    test_number_to_pair(5, 'White', 'Slate')
+    test_pair_to_number('Black', 'Orange', 12)
+    test_pair_to_number('Violet', 'Slate', 25)
+    test_pair_to_number('Red', 'Orange', 7)
+    print("All tests passed!\n")
+
+    print_reference_manual()
