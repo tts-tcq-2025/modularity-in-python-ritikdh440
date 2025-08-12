e7ca3d369a055c0cd0605a8fda81cad8ab6f1240
Ritik dhiman <57491718+ritikdh440@users.noreply.github.com>
2025-08-12 09:11:24 +0530

    Create color_map.py

---
+++ color_map.py
@@ -0,0 +1,7 @@
+"""
+This module contains the constant lists for major and minor colors
+used in the 25-pair color code.
+"""
+
+MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
